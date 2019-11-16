82d2eb887eb82674eacee2e8553d393f7f58aab5
刘海龙 <lhlmicheal@gmail.com>
2019-11-16 16:41:33 +0800

    "Melee pro create"

---
+++ Melee/Assets/Scripts/NavigationAutoTest.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NavigationAutoTest : MonoBehaviour
+{
+    public Transform targetObj;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (targetObj != null)
+        {
+            GetComponent<UnityEngine.AI.NavMeshAgent>().destination = targetObj.position;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
